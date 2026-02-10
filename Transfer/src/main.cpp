dfd065384318abd765662e6f00fcd89e0159d915
marco <marco@MARCO-PC>
2026-02-10 03:31:00 -0600

    commit baseline files

--- Transfer/src/main.cpp
+++ Transfer/src/main.cpp
@@ -1,24 +1,11 @@
- //SDL3 imports
+// File: Transfer/src/main.cpp
+//SDL3 imports
  #include "SDL3/SDL.h"
  #include "SDL3/SDL_events.h"
 
-
-// All main.cpp imports require full route
+// Custom Imports
 #include "Core/Game.h"
 
-
-
-// // Custom imports
-// #include "RenderHelpers.h"
-// #include "Colors.h"
-// #include "ActionHelpers.h"
-// #include "Bodies.h"
-// #include "EventListenerHelpers.h"
-// #include "RunGame.h"
-
-// // Standard library imports
-// #include <iostream>
-// #include <vector> 
 
  int main(int argcc, char* argv[])
  {
